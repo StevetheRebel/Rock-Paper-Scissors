4bd45ff725d30e9eb57a1bc98777ffb5a66def21
StevetheRebel <124534650+StevetheRebel@users.noreply.github.com>
2023-04-11 11:57:13 +0300

    Playing the First Round

--- myScript.js
+++ myScript.js
@@ -5,3 +5,25 @@ function getComputerChoice() {
 	return choice[randomIndex];
 }
 
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection === computerSelection.toLowerCase()) {
+		return "It's a tie!";
+	}
+	
+	if (
+		(playerSelection === 'rock' && computerSelection.toLowerCase() === 'scissors')
+	) {
+		return `You Win! ${playerSelection} beats ${computerSelection}.`;
+	} else {
+		return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+	};
+
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
