a6c489edbcc7e89498af546bea1d83e492b34292
StevetheRebel <124534650+StevetheRebel@users.noreply.github.com>
2023-04-13 14:21:44 +0300

    Whole game functions

--- myScript.js
+++ myScript.js
@@ -1,29 +1,54 @@
+// Random "Rock, Paper, and Scissors Computer Selection"
+
 function getComputerChoice() {
-	const choice = ['Rock', 'Paper', 'Scissors'];
+	const choice = ['rock', 'paper', 'scissors'];
 	const randomIndex = Math.floor(Math.random() * 3);
 
 	return choice[randomIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
-	if (playerSelection === computerSelection.toLowerCase()) {
-		return "It's a tie!";
-	}
-	
-	if (
-		(playerSelection === 'rock' && computerSelection.toLowerCase() === 'scissors')
-	) {
-		return `You Win! ${playerSelection} beats ${computerSelection}.`;
-	} else {
-		return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-	};
+	playerChoice = playerSelection.toLowerCase();
 
+	const drawMsg = "It's a tie!";
+	const winMsg = `You win! ${playerChoice} beats ${computerSelection}`;
+	const loseMsg = `You lose! ${computerSelection} beats ${playerChoice}`;
+
+	if (playerChoice === computerSelection.toLowerCase()) {
+		return drawMsg;
+	} else if (playerChoice === 'rock' && computerSelection === 'scissors' ||
+	playerChoice === 'paper' && computerSelection === 'rock' ||
+	playerChoice === 'scissors' && computerSelection === 'paper') {
+		return winMsg;
+	} else {
+		return loseMsg;
+	}
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
+function game() {
+	let compWinCount = 0;
+	let playerWinCount = 0;
 
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
 
+	for (i = 0; i <= 5; i++) {
+		// let gameRounds = prompt('How many rounds do you wanna play?');
+		let playerSelection = prompt('Choose Rock, Paper or Scissors?');
+		let computerSelection = getComputerChoice();
+		let result = playRound(playerSelection, computerSelection);
+		console.log(result);
+
+		if (result.includes("win")) {
+			playerWinCount++;
+		} else if (result.includes("lose")) {
+			compWinCount++;
+		}
+
+		if (playerWinCount >= gameRounds/2) {
+			console.log('You win!')
+		} else if (compWinCount >= gameRounds/2) {
+			console.log('You lose!')
+		} else {
+			console.log('You draw!')
+		}
+	}
+}
