f010f274b0f363b832de9dec069bdd9a92586c03
StevetheRebel <124534650+StevetheRebel@users.noreply.github.com>
2023-04-20 16:24:09 +0300

    Final

--- myScript.js
+++ myScript.js
@@ -1,56 +1,56 @@
 function getComputerChoice() {
-	const choice = ['rock', 'paper', 'scissors'];
-	const randomIndex = Math.floor(Math.random() * 3);
+  const choice = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
 
-	return choice[randomIndex];
+  return choice[randomIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
-	let playerChoice = playerSelection.toLowerCase();
+  let playerChoice = playerSelection.toLowerCase();
 
-	const drawMsg = "It's a tie!";
-	const winMsg = `You win! ${playerChoice} beats ${computerSelection}`;
-	const loseMsg = `You lose! ${computerSelection} beats ${playerChoice}`;
+  const drawMsg = "It's a tie!";
+  const winMsg = `You win! ${playerChoice} beats ${computerSelection}`;
+  const loseMsg = `You lose! ${computerSelection} beats ${playerChoice}`;
 
-	if (playerChoice === computerSelection.toLowerCase()) {
-		return drawMsg;
-	} else if (playerChoice === 'rock' && computerSelection === 'scissors' ||
-	playerChoice === 'paper' && computerSelection === 'rock' ||
-	playerChoice === 'scissors' && computerSelection === 'paper') {
-		return winMsg;
-	} else {
-		return loseMsg;
-	}
+  if (playerChoice === computerSelection.toLowerCase()) {
+    return drawMsg;
+  } else if (
+    (playerChoice === "rock" && computerSelection === "scissors") ||
+    (playerChoice === "paper" && computerSelection === "rock") ||
+    (playerChoice === "scissors" && computerSelection === "paper")
+  ) {
+    return winMsg;
+  } else {
+    return loseMsg;
+  }
 }
 
 function game(gamerRounds) {
-	let compWinCount = 0;
-	let playerWinCount = 0;
+  let compWinCount = 0;
+  let playerWinCount = 0;
 
-	let gameRounds = prompt('How many rounds do you wanna play?');
+  let gameRounds = prompt("How many rounds do you wanna play?");
 
-	for (let i = 0; i < gameRounds; i++) {
-		let playerSelection = prompt('Choose Rock, Paper or Scissors?');
-		let computerSelection = getComputerChoice();
-		let result = playRound(playerSelection, computerSelection);
-		console.log(result);
+  for (let i = 0; i < gameRounds; i++) {
+    let playerSelection = prompt("Choose Rock, Paper or Scissors?");
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
+    console.log(result);
 
-		if (result.includes("win")) {
-			playerWinCount++;
-		} else if (result.includes("lose")) {
-			compWinCount++;
-		}
-	}
+    if (result.includes("win")) {
+      playerWinCount++;
+    } else if (result.includes("lose")) {
+      compWinCount++;
+    }
+  }
 
-	if (playerWinCount > compWinCount) {
-		console.log('You win!')
-	} else if (compWinCount > playerWinCount) {
-		console.log('You lose!')
-	} else {
-		console.log('You draw!')
-	}
-	
-	console.log(compWinCount);
+  if (playerWinCount > compWinCount) {
+    console.log("You win!");
+  } else if (compWinCount > playerWinCount) {
+    console.log("You lose!");
+  } else {
+    console.log("You draw!");
+  }
 }
 
 game();
